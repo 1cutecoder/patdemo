2cab9ff97de9832a1841b9fb33004524571e465e
zcl <820861340@qq.com>
2022-01-19 10:51:51 +0800

    netty initial

--- nettydemo/src/main/java/server/service/ServiceFactory.java
+++ nettydemo/src/main/java/server/service/ServiceFactory.java
@@ -1,7 +1,12 @@
 package server.service;
 
+import config.Config;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -10,9 +15,29 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class ServiceFactory {
     static Properties properties;
-    static Map<Class<?>,Object> map = new ConcurrentHashMap<>();
+    static Map<Class<?>, Object> map = new ConcurrentHashMap<>();
 
     static {
+        InputStream in = Config.class.getClassLoader().getResourceAsStream("application.properties");
+        try {
+            properties = new Properties();
+            properties.load(in);
+            Set<String> names = properties.stringPropertyNames();
+            for (String name : names) {
+                if (name.endsWith("Service")) {
+                    Class<?> interfaceClass = Class.forName(name);
+                    Class<?> instanceClass = Class.forName(properties.getProperty(name));
+                    map.put(interfaceClass, instanceClass.newInstance());
+                }
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            throw new ExceptionInInitializerError(e);
+        } catch (IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public static  <T>T getService(Class<T> interfaceClass) {
+        return (T) map.get(interfaceClass);
     }
 }
